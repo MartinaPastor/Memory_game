57cb0986c1d46fcd35a2212fb8290bca63720c5f
Martina <47643931+MartinaPastor@users.noreply.github.com>
2020-09-04 00:45:31 +0200

    added description to special card

--- src/game/OldHag.java
+++ src/game/OldHag.java
@@ -3,6 +3,7 @@ package game;
 
 public class OldHag{
 	//special card with ID and Icon
+	// Inspired by the Italian character "la Befana", the trickster counterpart to Santa. 
 	
 	public static final String OLD_HAG_FRONT = "src/oldhag.jpg"; // image of old hag
 	public static final int OLD_HAG_ID = 12;
